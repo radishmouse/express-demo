c8952a8591cf46a3759af841bd17c9db05646ebc
Chris Aquino <chris.aquino@gmail.com>
2019-04-01 10:26:03 -0400

    initial files for demo

---
+++ index.js
@@ -0,0 +1,35 @@
+// Bring in the express module.
+const express = require('express');
+const PORT = 3000;
+
+// Create an express app.
+// It's roughly equivalent to the result of calling `http.createServer()`
+const app = express();
+
+// Respond to GET requests for the path "/"
+app.get('/', (req, res) => {
+    // Note: this is different from the plain `res.end`
+    res.send('Home page');
+});
+
+app.post('/', (req, res) => {
+    res.send('You sent a POST');
+});
+
+app.put('/', (req, res) => {
+    res.send('You sent a PUT');
+});
+
+app.delete('/', (req, res) => {
+    res.send('You sent a DELETE');
+});
+
+app.get('/bye', (req, res) => {
+    res.send('Byeeee');
+});
+
+
+// Listen on our PORT
+app.listen(PORT, () => {
+    console.log(`Your amazing express app is running on port ${PORT}`);
+});
